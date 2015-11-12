9828ca356672204c5017a80b26d451244feabade
denisromanov <denis.romonov@gmail.com>
2015-11-12 18:55:01 +0300

    Branche[Testing] create AdminController

---
+++ WebUI/Controllers/AdminController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using BookDB.Abstract;
+using BookDB.Entities;
+
+namespace WebUI.Controllers
+{
+    [Authorize]
+    public class AdminController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
